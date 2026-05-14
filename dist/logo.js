e23a4397b3421d62f7d88b593924db595bb10523
somil4632 <somil0999@gmail.com>
2026-05-14 12:31:59 +0530

    made the 3dots working

--- dist/logo.js
+++ dist/logo.js
@@ -381,3 +381,15 @@ function resizeLogo(){
 }
 window.addEventListener('resize',resizeLogo)
 resizeLogo();
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const open = document.getElementById('dot');
+    const item = document.getElementById('item');
+
+    open.addEventListener('click',() =>{
+    item.classList.toggle('hidden');
+    })
+   
+
+})
